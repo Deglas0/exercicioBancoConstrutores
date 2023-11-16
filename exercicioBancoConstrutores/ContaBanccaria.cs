6625a1b2e9dcd9811b57b824d9dcc10c869aeb7c
Douglas miguel <124850234+Deglas0@users.noreply.github.com>
2023-11-15 22:18:42 -0300

    Initial commit

---
+++ exercicioBancoConstrutores/ContaBanccaria.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace exercicioBancoConstrutores
+{
+    internal class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        private string _titular;
+        public double Saldo { get; private set; }
+
+
+
+
+
+        public string Titular
+        {
+            get { return _titular; }
+            set
+            {
+                if (value != null && value.Length > 3)
+                {
+                    _titular = value;
+                }
+            }
+        }
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            _titular = titular;
+        }
+
+        public ContaBancaria(int numero, string titular, double saldo) : this(numero, titular)
+        {
+
+            Saldo = saldo;
+        }
+
+        public void Saque(double Quantia)
+        {
+            Saldo -= Quantia-5.00;
+            Console.WriteLine("Seu saldo atual:\nR$"+Saldo);
+        }
+        public void Deposito(double Quantia)
+        {
+            Saldo += Quantia;
+            Console.WriteLine("Seu saldo atual:\nR$"+Saldo);
+        }
+
+        public override string ToString()
+        {
+            return "numero connta bancaria: " + Numero + "\n"
+                  + "Titular: " + _titular + "\n"
+                  + "Saldo atual: R$" + Saldo.ToString("f2",CultureInfo.InvariantCulture) + "\n";
+
+        }
+
+
+
+
+    }
+}
---
+++ exercicioBancoConstrutores/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace exercicioBancoConstrutores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta = null;
+
+            Console.WriteLine("Olá, bem vindo ao Banco con5trut0rs\n" +
+                              "vejo que não há uma conta vinculada, porfavor crie uma conta:");
+
+            Console.WriteLine("Digite o numero da conta:");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite seu nome:");
+            string titular = Console.ReadLine();
+            Console.WriteLine("ok, tudo certo!!\n" +
+                              "Agora preciso saber se você quer começar com um deposito inicial\n" +
+                              "ou não?\n" +
+                              "s/S = sim\n" +
+                              "n/N = não");
+
+            string escolha;
+            escolha = Console.ReadLine();
+            if (escolha == "s" || escolha == "S")
+            {
+                Console.WriteLine("Quanto seria o deposito iiccial: ");
+                double saldo = double.Parse(Console.ReadLine());
+                conta = new ContaBancaria(numero, titular, saldo);
+            }
+            else if (escolha == "n" || escolha == "N")
+            {
+                Console.WriteLine("ok, então vamos continnuar" +
+                                  "╰(*°▽°*)╯");
+
+                conta = new ContaBancaria(numero, titular);
+            }
+            else
+            {
+                Console.WriteLine("opção invalida, porfavor digite uma opção valida");
+            }
+            Console.WriteLine(conta); Console.WriteLine(conta);
+
+            int iterator = 0;
+            
+            while (iterator == 0)
+            {
+
+                int sw = int.Parse(Console.ReadLine());
+                if (sw == null) { sw = 0; }
+                switch (sw)
+                {
+                    case 0:
+                        Console.WriteLine(conta);
+                        break;
+                    case 1:
+                        Console.WriteLine("sacar!!\n" +
+                                          "digite o valor que deseja sacar:");
+                        double s1 = double.Parse(Console.ReadLine());
+                        conta.Saque(s1);
+                        break;
+                    
+                    case 2:
+                        Console.WriteLine("depositar!!\n+" +
+                                          "digite o valor que deseja depositar:");
+                        double d1 = double.Parse(Console.ReadLine());
+                        conta.Deposito(d1);
+                        break;     
+                     
+                    case 3:
+                        Console.WriteLine("Obrigado por usar o Banco Con5trut0rs\n" +
+                                          "Volte sempre༼ つ ◕_◕ ༽つ");
+                        break;
+
+                }
+            }
+
+        }
+    }
+}
